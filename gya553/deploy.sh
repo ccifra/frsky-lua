404b8077bc90d5a51f15e9f9c044159f492ff123
Christopher Cifra <christopher.cifra@emerson.com>
2026-02-04 08:16:59 -0600

    File location cleanup

--- gya553/deploy.sh
+++ gya553/deploy.sh
@@ -7,13 +7,14 @@ DEST_DIR="/Applications/Ethos Suite.app/Contents/.simulator/1.6.3/persist/X20PRO
 # Create destination directory if it doesn't exist
 mkdir -p "$DEST_DIR"
 mkdir -p "$DEST_DIR/gya553-status"
+mkdir -p "$DEST_DIR/gya553-gains"
 
 # Delete old compiled versions
-rm -f "$DEST_DIR/gya553-gains.luac"
+rm -f "$DEST_DIR/gya553-gains/main.luac"
 rm -f "$DEST_DIR/gya553-status/main.luac"
 
 # Copy the new combined script
-cp gya553-gains.lua "$DEST_DIR/"
 cp -r gya553-status/* "$DEST_DIR/gya553-status/"
+cp -r gya553-gains/* "$DEST_DIR/gya553-gains/"
 
 echo "Deployed GYA553 scripts to simulator/scripts"
